a813bd6099db5241110f6ec2fc9c35b968bdfe75
AshishCh11 <aashishchavan309@gmail.com>
2023-02-18 01:10:23 +0530

    Update MissingNo.java

--- src/MissingNo.java
+++ src/MissingNo.java
@@ -3,19 +3,15 @@ public class MissingNo {
 	
 	 public static void main(String args[]) 
 	 {
-		 int [] a={1,2,4,5,6};
-		 int sum=0,i;
-		 int exno=a.length+1;
-		 int n=exno+1;
-		 int tsum=(exno*n)/2;
-		 
-		 for(i=0;i<a.length;i++)
-		 {
-			 sum=sum+a[i];
-		 }
-		 System.out.println(tsum-sum);
-		 
-		 
-		 
+		int[] a = { 1, 2, 3, 5, 6, 7 };
+		int missingNo = 0;
+
+		for (int i = 0; i < a.length; i++) {
+			if (i < a.length - 1) {
+				if (a[i + 1] - a[i] > 1)
+					missingNo = a[i] + 1;
+			}
+		}
+		System.out.println(missingNo);	 
 	 }
 }
