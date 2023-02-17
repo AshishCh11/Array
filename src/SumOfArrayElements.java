e90c26bbf7757390ccdac3d63d284cf91238826e
AshishCh11 <aashishchavan309@gmail.com>
2023-02-18 01:41:53 +0530

    Update SumOfArrayElements.java

--- src/SumOfArrayElements.java
+++ src/SumOfArrayElements.java
@@ -1,15 +1,15 @@
 
 public class SumOfArrayElements {
-	public static void main(String args[])
-	   
-	 public static void main(String[] args) {
-		int[] a = { 10, 25, 30, 40 };
-		int sum = 35;
+	public static void main(String[] args) {
+		int[] a = { 10, 25, 30, 40, 2 };
+		int sum = 32;
 		for (int i = 0; i < a.length - 1; i++) {
-			if (a[i] + a[i + 1] == sum) {
-				System.out.println(a[i] + " " + a[i + 1]);
+			for (int j = i + 1; j < a.length; j++) {
+				if (a[i] + a[j] == sum)
+					System.out.println(a[i] + " " + a[j]);
 			}
 		}
 	}
 }
+
 
