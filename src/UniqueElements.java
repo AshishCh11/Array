667ccd922ecbe9dff88f0a127d0ef5d8a11a0338
AshishCh11 <aashishchavan309@gmail.com>
2023-02-18 17:29:21 +0530

    Update UniqueElements.java

--- src/UniqueElements.java
+++ src/UniqueElements.java
@@ -2,24 +2,10 @@ import java.util.Arrays;
 
 class UniqueElements
     {
-	 public static void main(String[] art) {
-		    int a[] = { 1,2,1,2,4,5,6};  //1 1 2 2 4 5 6 
-		    Arrays.sort(a);
-		    System.out.println(Arrays.toString(a));
-		    for (int j = 0; j < a.length; j++) {
-		        if(j==0) {
-		            if(a[j]!=a[j+1]) {
-		                System.out.println("The unique number is :"+a[j]);
-		            }
-		        }else
-		        if(j==a.length-1) {
-		            if(a[j]!=a[j-1]) {
-		                System.out.println("The unique number is :"+a[j]);
-		            }
-		        }else
-		        if(a[j]!=a[j+1] && a[j]!=a[j-1]) {
-		            System.out.println("The unique number is :"+a[j]);
-		        }
-		    }
-	 }
-    }
+	public static void main(String[] args) {
+		int[] a = { 5, 3, 2, 3, 9, 5 }; // 2 3 3 5 5 9
+		int[] b = Arrays.stream(a).distinct().toArray();
+		Arrays.stream(b).forEach(o -> System.out.println(o));
+
+	}
+}
